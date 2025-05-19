e4b431dd9f1d8e01be5fe2eaf14fd3004be8f229
Ferilax <muhammadfx@mail.ru>
2025-05-19 18:39:56 +0300

    fix

--- js/recomendation.js
+++ js/recomendation.js
@@ -8,17 +8,19 @@ function syncScroll(source, target) {
 	if (isSyncing) return;
 	isSyncing = true;
 
-	// Синхронизация скролла
 	target.scrollLeft = source.scrollLeft;
 
 	isSyncing = false;
 }
 
-// Обработчики событий
-tableBody.addEventListener('scroll', () => {
-	syncScroll(tableBody, tableHead);
-});
+// События для мгновенной синхронизации во время свайпа
+tableBody.addEventListener("touchmove", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("touchmove", () => syncScroll(tableHead, tableBody));
 
-tableHead.addEventListener('scroll', () => {
-	syncScroll(tableHead, tableBody);
-});
+// События для синхронизации после завершения инерции
+tableBody.addEventListener("touchend", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("touchend", () => syncScroll(tableHead, tableBody));
+
+// События для синхронизации во время обычного скролла
+tableBody.addEventListener("scroll", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("scroll", () => syncScroll(tableHead, tableBody));
