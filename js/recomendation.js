ee7f7525f04a4ae9504010b12acad37ee952fc66
Ferilax <muhammadfx@mail.ru>
2025-05-19 19:16:40 +0300

    fix

--- js/recomendation.js
+++ js/recomendation.js
@@ -2,75 +2,26 @@ const recomendationPage = document.querySelector(".recomendation-page");
 const tableBody = recomendationPage.querySelector(".table__head");
 const tableHead = recomendationPage.querySelector(".table__body");
 
-var scryll = function scryll(selector1, selector2, config) {
+let isSyncing = false;
 
-	var isEmpty = function isEmpty(val) {
-		return val === undefined || val === null;
-	};
+function syncScroll(source, target) {
+	if (isSyncing) return;
+	isSyncing = true;
 
-	var getElement = function getElement(val) {
-		var el = document.querySelector(val);
-		return el;
-	};
+	requestAnimationFrame(() => {
+		target.scrollLeft = source.scrollLeft;
+		isSyncing = false;
+	});
+}
 
-	config = Object.assign({}, {
-		init: true,
-		direction: 'vertical'
-	}, config);
+// События для мгновенной синхронизации во время свайпа
+tableBody.addEventListener("touchmove", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("touchmove", () => syncScroll(tableHead, tableBody));
 
-	function onElement1Scroll(event) {
-		var element2 = getElement(selector2);
+// События для синхронизации после завершения инерции
+tableBody.addEventListener("touchend", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("touchend", () => syncScroll(tableHead, tableBody));
 
-		if (config.direction === 'vertical') {
-			element2.scrollTop = event.target.scrollTop;
-		} else {
-			element2.scrollLeft = event.target.scrollLeft;
-		}
-	}
-
-	function onElement2Scroll(event) {
-		var element1 = getElement(selector1);
-
-		if (config.direction === 'vertical') {
-			element1.scrollTop = event.target.scrollTop;
-		} else {
-			element1.scrollLeft = event.target.scrollLeft;
-		}
-	}
-
-	var init = function init() {
-		if (isEmpty(selector1) || isEmpty(selector2)) {
-			return;
-		}
-
-		var element1 = getElement(selector1);
-		var element2 = getElement(selector2);
-
-		if (isEmpty(element1) || isEmpty(element2)) {
-			return;
-		}
-		element1.addEventListener('scroll', onElement1Scroll);
-		element2.addEventListener('scroll', onElement2Scroll);
-	};
-
-	var kill = function kill() {
-		var element1 = getElement(selector1);
-		var element2 = getElement(selector2);
-		element1.removeEventListener('scroll', onElement1Scroll);
-		element2.removeEventListener('scroll', onElement2Scroll);
-	};
-
-	if (config.init === true) {
-		init();
-	}
-
-	return {
-		init: init,
-		kill: kill
-	};
-};
-const myScryll = scryll(
-	".recomendation-page .table__head",
-	".recomendation-page .table__body",
-	{ direction: 'horizontal' }
-);
+// События для синхронизации во время обычного скролла
+tableBody.addEventListener("scroll", () => syncScroll(tableBody, tableHead));
+tableHead.addEventListener("scroll", () => syncScroll(tableHead, tableBody));
